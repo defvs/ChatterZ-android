0007088d5de4afd68cc145dbca2e6adceb6aaaed
Daniel THIRION <defvs.daniel@gmail.com>
2020-07-13 22:42:57 +0200

    Create login activity

--- app/src/main/java/dev/defvs/chatterz/TwitchLoginActivity.kt
+++ app/src/main/java/dev/defvs/chatterz/TwitchLoginActivity.kt
@@ -4,8 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class TwitchLoginActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_twitch_login)
-    }
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_twitch_login)
+	}
 }
