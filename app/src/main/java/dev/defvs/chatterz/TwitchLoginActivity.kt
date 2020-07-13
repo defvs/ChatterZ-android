964d6e6197ec76a07d726709210feb569ed5f932
Daniel THIRION <defvs.daniel@gmail.com>
2020-07-13 22:42:51 +0200

    Add hints and content descriptors to main activity

---
+++ app/src/main/java/dev/defvs/chatterz/TwitchLoginActivity.kt
@@ -0,0 +1,11 @@
+package dev.defvs.chatterz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TwitchLoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_twitch_login)
+    }
+}
